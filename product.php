73b5f8640320e9ae55bdf50e897978117d460b32
Matas <92822344+MatasLuc@users.noreply.github.com>
2026-01-05 21:03:43 +0200

    Update product.php

--- product.php
+++ product.php
@@ -250,7 +250,7 @@ $meta = [
 
         <div class="info-grid">
           <?php if ($isFreeShippingGift): ?>
-            <div class="info-card" style="display:flex; align-items:center; gap:8px; background:#829ed6; border-color:#829ed6; color:#000;">
+            <div class="info-card" style="display:flex; align-items:center; gap:8px; background:#829ed6; border-color:#829ed6; color:#fff;">
               <span>🎁Pirkite šią prekę ir gausite nemokamą viso užsakymo pristatymą dovanų.</span>
             </div>
           <?php endif; ?>
